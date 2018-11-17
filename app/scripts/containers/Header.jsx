64185d4acc14e9f81c3fb50e5e79f837334e4fe3
Ursula Cervantes <ursula.cervantes@gmail.com>
2018-11-17 16:10:22 -0500

    style(header): add css to header menu

--- app/scripts/containers/Header.jsx
+++ app/scripts/containers/Header.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import { logOut } from 'actions';
 
 export default class Header extends React.PureComponent {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-  };
 
   render() {
     return (
       <header className="app__header">
         <div className="app__container">
-          Data Gate
+          <a className="title">Data Gate</a>
           <div className="app__header__menu">
             <ul className="list-unstyled">
               <li>
-                <a href="#review" className="app__review">
+                <a href="#">
                   <span>Review</span>
                 </a>
               </li>
               <li>
-                <a href="/request" className="app__request">
+                <a href="/request" className="active">
                   <span>Request</span>
                 </a>
               </li>
               <li>
-                <a href="/manage" className="app__manage">
+                <a href="/manage">
                   <span>Manage</span>
                 </a>
               </li>
