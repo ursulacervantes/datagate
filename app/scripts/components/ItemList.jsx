14e44adb284cdc9dfe3d8c62036aca60ba65dc56
Ursula Cervantes <ursula.cervantes@gmail.com>
2018-11-18 10:22:39 -0500

    style(manage): add sttate to shighlight selected item on side menu

--- app/scripts/components/ItemList.jsx
+++ app/scripts/components/ItemList.jsx
@@ -12,6 +12,10 @@ export class ItemList extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      active: ''
+    };
   }
 
   componentDidMount() {
@@ -19,9 +23,16 @@ export class ItemList extends React.Component {
     dispatch(getItemList());
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.items.length > 0) {
+      this.setState({ active: nextProps.items[0].id });
+    }
+  }
+
   handleClick = (e) => {
     e.preventDefault();
     this.props.handleItemSelected(e.target.parentElement.id);
+    this.setState({ active: e.target.parentElement.id });
   };
 
   render() {
@@ -29,10 +40,12 @@ export class ItemList extends React.Component {
       <div key="ItemList" className="app__item-list">
           <ul className="nav flex-column nav-pills side-menu">
             { !!this.props.items && this.props.items.map(v => (
-                <li key={v.id} id={v.id}
-                    className="nav-item"
+                <li key={v.id} id={v.id} className="nav-item"
                     onClick={this.handleClick.bind(this)}>
-                  <a className="nav-link">{v.name}</a>
+                  <a className={this.state.active === v.id ?
+                            'nav-link active' : 'nav-link'}>
+                    {v.name}
+                  </a>
                 </li>
             ))}
           </ul>
