3ec0e20258e34a68532a823ea623be21403e91d4
Ursula Cervantes <ursula.cervantes@gmail.com>
2018-11-18 23:54:20 -0500

    chore(web.js): create port variable

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var port = process.env.PORT || 5000;
 
- app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
 });
